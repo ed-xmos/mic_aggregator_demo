029c3e4345754c096beabdda62e57d0c914d589b
Ed <ed@xmos.com>
2023-07-14 10:32:50 +0100

    WIP add mic array

---
+++ app_mic_aggregator/src/app_config.h
@@ -0,0 +1,46 @@
+// Copyright 2023 XMOS LIMITED.
+// This Software is subject to the terms of the XMOS Public Licence: Version 1.
+
+#pragma once
+
+#define AUDIO_BUFFER_SAMPLES  ((unsigned)(MIC_ARRAY_CONFIG_SAMPLES_PER_FRAME * 1.2))
+#define APP_AUDIO_PIPELINE_SAMPLE_RATE  16000
+#define APP_I2S_AUDIO_SAMPLE_RATE       APP_AUDIO_PIPELINE_SAMPLE_RATE
+
+#define USE_BUTTONS                     1
+#define ENABLE_BURN_MIPS                0           // Specifies whether to include burn() tasks on remaining cores for testing purposes.
+
+
+#define MIC_ARRAY_CONFIG_MCLK_FREQ      24576000
+#define MIC_ARRAY_CONFIG_PDM_FREQ       3072000
+#define MIC_ARRAY_CONFIG_USE_DDR        1
+#define MIC_ARRAY_CONFIG_PORT_MCLK      XS1_PORT_1D // X0D11, J14 - Pin 15
+#define MIC_ARRAY_CONFIG_PORT_PDM_CLK   XS1_PORT_1A // X0D00, J14 - Pin 2
+#define MIC_ARRAY_CONFIG_PORT_PDM_DATA  XS1_PORT_8B // X0D14..X0D21 | J14 - Pin 3,5,12,14 and Pin 6,7,10,11
+#define MIC_ARRAY_CONFIG_MIC_COUNT      16
+#define MIC_ARRAY_CONFIG_SAMPLES_PER_FRAME 1
+#define MIC_ARRAY_TILE                  0           // NOTE: Tile 1 might still have issues with channels other than the first.
+#define NUM_DECIMATOR_SUBTASKS          2           // Indicates the number of subtasks to perform the decimation process on.
+
+
+#define MIC_ARRAY_CLK1                  XS1_CLKBLK_1
+#define MIC_ARRAY_CLK2                  XS1_CLKBLK_2
+                
+
+
+// Configuration checks
+#if MIC_ARRAY_CONFIG_MIC_COUNT > 8 && NUM_DECIMATOR_SUBTASKS < 2
+#error "NUM_DECIMATOR_SUBTASKS: Unsupported value"
+#endif
+
+#if !(MIC_ARRAY_CONFIG_MIC_COUNT == 8 || MIC_ARRAY_CONFIG_MIC_COUNT == 16)
+#error "MIC_ARRAY_CONFIG_MIC_COUNT: Unsupported value"
+#endif
+
+#if NUM_DECIMATOR_SUBTASKS > MIC_ARRAY_CONFIG_MIC_COUNT
+#error "NUM_DECIMATOR_SUBTASKS must be less than or equal to MIC_ARRAY_CONFIG_MIC_COUNT"
+#endif
+
+#if MIC_ARRAY_CONFIG_USE_DDR != 1
+#error "MIC_ARRAY_CONFIG_USE_DDR: This application only supports DDR"
+#endif
--- app_mic_aggregator/src/app_main.c
+++ app_mic_aggregator/src/app_main.c
@@ -1,15 +1,36 @@
+// Copyright 2023 XMOS LIMITED.
+// This Software is subject to the terms of the XMOS Public Licence: Version 1.
+
 #include <stdio.h>
 #include <xcore/channel.h>
 #include <xcore/parallel.h>
 
-DECLARE_JOB(pdm_mic_16, (void));
-void pdm_mic_16(void) {
+#include "app_config.h"
+#include "mic_array.h"
+#include "device_pll_ctrl.h"
+
+void app_mic_array_init( void );
+void app_mic_array_task( chanend_t c_frames_out );
+
+DECLARE_JOB(pdm_mic_16, (chanend_t));
+void pdm_mic_16(chanend_t c_mic_array) {
     printf("pdm_mic_16\n");
+
+    app_mic_array_init();
+    app_mic_array_task(c_mic_array);
 }
 
-DECLARE_JOB(hub, (void));
-void hub(void) {
+DECLARE_JOB(hub, (chanend_t));
+void hub(chanend_t c_mic_array) {
     printf("hub\n");
+
+    int32_t audio_frame[MIC_ARRAY_CONFIG_SAMPLES_PER_FRAME * MIC_ARRAY_CONFIG_MIC_COUNT];
+
+    while(1){
+
+      ma_frame_rx(audio_frame, c_mic_array, MIC_ARRAY_CONFIG_MIC_COUNT, MIC_ARRAY_CONFIG_SAMPLES_PER_FRAME);
+      printf("ma_frame_rx\n");
+  }
 }
 
 DECLARE_JOB(tdm16, (void));
@@ -31,15 +52,17 @@ void main_tile_0(chanend_t c_cross_tile){
     printf("Hello world tile[0]\n");
 
     PAR_JOBS(
-        PJOB(pdm_mic_16, ())
+        PJOB(pdm_mic_16, (c_cross_tile))
     );
 }
 
 void main_tile_1(chanend_t c_cross_tile){
     printf("Hello world tile[1]\n");
 
+    device_pll_init();
+
     PAR_JOBS(
-        PJOB(hub, ()),
+        PJOB(hub, (c_cross_tile)),
         PJOB(tdm16, ()),
         PJOB(tdm_master_emulator, ())
     );
